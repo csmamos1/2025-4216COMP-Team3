3da0ee564285ecd18e5fef08eda3ed96667aa77b
peterop23 <49170425+peterop23@users.noreply.github.com>
2025-04-01 11:38:00 +0100

    Add files via upload

---
+++ first.py
@@ -0,0 +1,12 @@
+import pandas
+import matplotlib.pyplot as plt
+import csv#rainfall and sun
+with open(MET Office Weather Data.csv) as f:
+    csv_reader = csv.reader(f)
+    next(csv_reader)
+    
+    C = []
+    for row in csv_reader:
+    data = float 
+    
+    
