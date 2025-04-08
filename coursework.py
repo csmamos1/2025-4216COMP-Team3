0d2c7af3ed847b5747f8b041169807e1e1a3d19a
Pet7r <PALP1_23@UNI.WORC.AC.UK>
2025-04-08 17:14:33 +0100

    update

--- coursework.py
+++ coursework.py
@@ -275,6 +275,41 @@ def Benson():
 
         # Show plot
         plt.show()
+        
+def Peter():
+    rainfall = []
+    sun = []
+    with open('MET Office Weather Data.csv','r') as f:#Opens csv file
+        csv_reader = csv.reader(f)#allows for data to be read
+        headers=next(csv_reader)
+        rainfall_idx = headers.index('rain')
+        sun_idx = headers.index('sun')
+    
+        for row in csv_reader:  #For NA values 
+            rain_val = row[rainfall_idx]#done so it knows which row to follow for rain
+            rainfall.append(float(rain_val) if rain_val != 'NA' else None)#makes sure na values are not shown
+            
+            
+            sun_val = row[sun_idx]#done so it knows which row to follow for sun
+            sun.append(float(sun_val) if sun_val != 'NA' else None)
+            
+
+            
+    valid_pairs = [(r, s) for r, s in zip(rainfall, sun) if r is not None and s is not None]#only allows valid pairs to be shown in grap
+    rain_valid = [pair[0] for pair in valid_pairs]
+    sun_valid = [pair[1] for pair in valid_pairs]
+
+    plt.subplot(2, 1, 2)#plots the graph
+    scatter = plt.scatter(rain_valid, sun_valid, c=range(len(rain_valid)), 
+                        cmap='viridis', alpha=0.7, s=100, edgecolor='w')
+
+
+    plt.title('Rainfall vs Sunshine Correlation', pad=10, fontsize=14)#adds the labels to the graph
+    plt.xlabel('Rainfall (mm)', fontsize=12)
+    plt.ylabel('Sunshine (hours)', fontsize=12)
+
+    plt.tight_layout()
+    plt.show()
 
 
 
@@ -289,7 +324,8 @@ while True:
     "year visualization \n and average minimum temperature againt rainfall \n"
     "type \033[1mAbdulla()\033[0m if you wish to air frost and sun\n"
     "type \033[1mJake()\033[0m if you wish to average hours of sunlight against months\n"
-    "type \033[1mBenson()\033[0m if you wish to see rainfall against months data ")
+    "type \033[1mBenson()\033[0m if you wish to see rainfall against months data \n"
+    "type \033[1mPeter()\033[0m if you wish to see the correlation between sunshine and rain")
     if Visualisation == 'quit':
         break
 
@@ -307,6 +343,12 @@ while True:
     if Visualisation.lower()=="benson()":   
         
         Benson()
+        
+    if Visualisation.lower()=="peter()":   
+        
+        Peter()
 
     else:
         print("There is no data matching this imput") 
+        
+        
