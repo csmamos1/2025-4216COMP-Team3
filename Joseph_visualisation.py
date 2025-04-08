4a94e447875f665ad298354233776dc835e23308
jjrose176@gmail.com <jjrose176@gmail.com>
2025-04-08 01:31:55 +0100

    update with comments

--- Joseph_visualisation.py
+++ Joseph_visualisation.py
@@ -4,7 +4,7 @@ import csv
 import matplotlib.pyplot as plt
 
 data=pd.read_csv('MET Office Weather Data.csv')
-
+# imported do manipulate the grid ofr my visulaization
 
 def joseph():
 
@@ -12,9 +12,15 @@ def joseph():
     new_data_Hurn = data[data["rain"].notna() & (data["station"]=="hurn") & 
                     (data["tmin"].notna())& (data["year"]!="1965.0")]
                 
+    #print (new_data)
 
-    pd.set_option("display.max_rows", None)  # force python to display all rows and columns
-    pd.set_option("display.max_columns", None)  
+
+   
+
+
+
+    pd.set_option("display.max_rows", None)  # Show all rows
+    pd.set_option("display.max_columns", None)  # Show all columns
 
     #print(new_data)
 
@@ -24,10 +30,32 @@ def joseph():
 
     new_data1= new_data0[(new_data0["year"]<2020.0)]
 
+    
+
+    #print(len(new_data1)) # want to find how many months in total in my data set
+    #after finding there 753months 753/12 gives float, means some years must have less months
+
+
+
+
     year_counts = new_data1["year"].value_counts()
     year_range = range(1957, 2020)  
     year_counts = year_counts.reindex(year_range, fill_value=0)
-   
+    #print(year_counts)
+    # so i counted how many months each year had and found that 2009,2015,2016 all have 11months
+    #this makes sense because (63*12=756)-3 = 753, there are three months missing so i forced pandas to show all
+    #the whole data set and founf that febuary is missing in each case, i want to include this analysis to show
+    #a margin of error.
+    #total_months = 64 * 12  # Expected months
+    #missing_months = 3
+    #observed_months = total_months - missing_months
+    #issing_percentage = (missing_months / total_months) * 100
+    #print(f"Missing Data Percentage: {missing_percentage:.2f}%")
+
+
+
+
+
     tmin_range=new_data1["tmin"]       
     tmin_averages=[]
     for start in range(0, 753, 12):
@@ -55,20 +83,21 @@ def joseph():
     print(f"\n The average minimum temperature from the years 1957-2019 in Hurn is \n  "
           f"{mean_tmin_averages:.2f} celcius and the standard deviation"
            f" (that is how much the the data deviates from the mean)\n"
-            f" is {std_dev_tmin:.2f} celsius thus each point of data deviates from the mean(Minimum teperature between 1957-2019)" 
-            f" by about {CV_tmin:.2f}% \n"
+            f" is {std_dev_tmin:.2f} celsius thus each point of data deviates from the mean(Minimum teperature between 1957-2019) by about {CV_tmin:.2f}% \n"
             f" which is a very negligible and shows a tight consistency between the data points \n"
             f" addiditonally there is a {missing_percentage:.2f}% error of the new data relative to the original")
     
     print(f"\n The average amount of rainfall from the years 1957-2019 in Hurn is \n  "
           f"{mean_rain_averages:.2f}mm and the standard deviation"
            f" (that is how much the the data deviates from the mean)\n"
-            f" is {std_dev_rain:.2f}mm  thus each point of data deviates from the mean(Minimum teperature between 1957-2019)"
-             f" by about {CV_rain:.2f}% \n "
+            f" is {std_dev_rain:.2f}mm  thus each point of data deviates from the mean(Minimum teperature between 1957-2019) by about {CV_rain:.2f}% \n "
             f" which is a moderate value and shows that the data points a relatively consistent with"
              f" the overall average. \n addiditonally there is a {missing_percentage:.2f}%"
              f"error of the new data relative to the original")
+        
+
     
+
     range_years= list(range(1957,2020))
     ###print(range_years)
 
@@ -115,42 +144,42 @@ def joseph():
 
     x=np.arange(1957, 2021)
     ax.set_xticklabels(range_years, fontsize=8)
-    ax.set_xticklabels(range_years, fontsize=8, rotation=45)  # Adjust font size here
+    ax.set_xticklabels(range_years, fontsize=8, rotation=45)  # Adjust fontsize and rotation
     ax.set_yticklabels(ax.get_yticks(), fontsize=8)
-    ax.set_xlim(x.min()-1, x.max())  # Ensures no extra padding at the edges
+    ax.set_xlim(x.min()-1, x.max())  # delete extra space at the end of grid
     mean_val = np.mean(rain_averages)
     plt.axhline(mean_val, color='red', linestyle='--', linewidth=2, label='Mean')
     ax.legend()
+    
+   
+
+
     plt.show()
 
 
 
     fig, ax = plt.subplots()
-    fig.suptitle("Minimum average temperature \n against average rainfall against year", fontsize=18)
     ax.plot(range_years, tmin_averages, 'mD:')
     ax.set_ylabel("Average Minimum temperature", color='m')
     ax1 = ax.twinx()
     ax1.plot(range_years, rain_averages, 'ro--')
     ax1.set_ylabel("Average rainfall", color='r')
-    
+
     plt.show()
 
-
-
-    print(f"{min(rain_averages):.2f}")  
+    print(f"{min(rain_averages):.2f}")
     print(f"{max(rain_averages):.2f}")
 
 
-    
 
 while True:
-    Visualization = input("\n please type \033[1mJoseph()\033[0m to see rain against year visualization, " \
-    "\n Aaverage minimum temperature against "
-    "year visualization \n and average minimum temperature againt rainfall")
+    Visualization = input("\n please type Joseph() to see rain against year visualization, " \
+    "\n Aaverage minimum temperature against ye"
+    "year visualization \n and average minimum temperature again")
     if Visualization == 'quit':
         break
 
     if Visualization.lower() == "joseph()":
         joseph()
     else:
-        print("There is no data matching this imput") 
+        print("There is no data matching this imput")
